4b0789813cce7ed7e70ab6c6abd4c93a2aaf8675
Dilbara Asanalieva <dilbara2025@gmail.com>
2021-07-08 17:52:39 +0600

    'lesson4'

--- lesson4_hw.py
+++ lesson4_hw.py
@@ -1,12 +1,25 @@
 '''Сделать список задач программу которая принимает 2 инпута (как в шаблоне)
 1 показать, 2 добавить задачу. Задача состоит из 2 обязательных полей
 1 сама задача и 2 дэдлайн до какого числа нужно сделать'''
+
 todo = []
-for i in range(100):
-    option = input('Write task and deadline ')
-    todo.append(option)
-    if option == 'end': #to see your todos write 'end'
-        for i in todo:
-            print(i)
+while True:
+     option = int(input('Write 1 to add todos  Write 0 to see todos '))
+
+     if option == 0:  #False
+         for i in todo:
+              print('Task:',i[0],'Added date:',i[1],'Deadline:',i[2])
+
+
+     if option == 1:  #True
+        todos = input('Write tasks ')
+        date = input('Write date ')
+        deadline = input('Write deadline ')
+        todo.append([todos, date, deadline])
+        print('Ready!')
+
+
+
+
 
 
